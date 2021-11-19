1f04e142c465ae5152304e6e4c16bf7d40057cee
Owen Utley <90651143+owenutley@users.noreply.github.com>
2021-11-19 16:23:37 -0700

    data types file added

---
+++ data_types.py
@@ -0,0 +1,24 @@
+x = 5
+print("x = 5 is\n", type(x), "\n")
+
+
+x = "Words"
+print("x = 'Words' is\n", type(x), "\n")
+
+
+x = 8.5
+print("x = 8.5 is\n", type(x), "\n")
+
+
+x = ['item1', 'item2', 'item3']
+print("x = ['item1', 'item2', 'item3'] is\n", type(x), "\n")
+
+
+x = ('item1', 'item2', 'item3')
+print("x = ('item1', 'item2', 'item3') is\n", type(x), "\n")
+
+x = {'one': 1, 'two': 2, 'three': 3}
+print("x = {'one': 1, 'two': 2, 'three': 3} is\n", type(x), "\n")
+
+x = True
+print("x = True is\n", type(x), "\n")
